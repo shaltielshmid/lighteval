9a7c5377e15826b4c6149b52f544f02d15d4af59
Shaltiel Shmidman <shaltieltzion@gmail.com>
2024-05-27 23:01:02 +0300

    Added image url parameter

--- src/lighteval/models/endpoint_model.py
+++ src/lighteval/models/endpoint_model.py
@@ -93,7 +93,7 @@ class InferenceEndpointModel(LightevalModel):
                             "MODEL_ID": "/repository",
                             **config.get_dtype_args(),
                         },
-                        "url": "ghcr.io/huggingface/text-generation-inference:2.0.3",
+                        "url": (config.image_url or "ghcr.io/huggingface/text-generation-inference:1.4.5"),
                     },
                 )
             hlog("Deploying your endpoint. Please wait.")
